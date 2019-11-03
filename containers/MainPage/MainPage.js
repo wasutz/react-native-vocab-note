9b4d183dde16e1899bd3ca491f23cdcc03d81bfb
Wasut Panyawiphat <wasut_pao@hotmail.com>
2019-11-04 00:52:17 +0700

    implement list vocabs with just Text component

--- containers/MainPage/MainPage.js
+++ containers/MainPage/MainPage.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Container, Text } from 'native-base';
+import { Container, Text, Toast } from 'native-base';
+import VocabStore from '../../stores/VocabStore';
+import {observer} from 'mobx-react';
 
 class MainPage extends React.Component {
   static navigationOptions = {
     title: 'Vocab Note'
   };
 
+  componentDidMount() {
+    VocabStore.getVocabs().catch(ex => {
+      if (ex.response.status === 401) {
+        this.props.navigation.navigate('Auth');
+      } else {
+        Toast.show({
+          text: 'Something went wrong',
+          buttonText: 'Okay',
+          type: 'danger',
+          duration: 3000
+        });
+      }
+    });
+  }
+
   render() {
+    const {vocabs} = VocabStore;
+
     return (
       <Container>
-        <Text>Main Page</Text>
+        {vocabs.map(vocab => (
+          <Text key={vocab.id}>{vocab.word}</Text>
+        ))}
       </Container>
     );
   }
 }
 
-export default MainPage;
+export default observer(MainPage);
